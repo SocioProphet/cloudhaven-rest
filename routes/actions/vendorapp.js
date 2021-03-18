134c212716175fef72116dd4e632ee86b9300d6f
richjvann <richjvann@gmail.com>
2021-03-18 10:05:34 -0700

    improvements to demo vendor app

--- routes/actions/vendorapp.js
+++ routes/actions/vendorapp.js
@@ -11,12 +11,12 @@ const dataModel = {
 
       ],
       formData: {
-        textField1: '',
-        textField2: '',
+        firstName: '',
+        lastName: '',
         textField3: '',
         textField4: ''
       },
-      tab: 1,
+      tab: 0,
       tabItems: ['tab1', 'tab2', 'tab3']
     };
 const uiMethods = {
@@ -25,198 +25,225 @@ const uiMethods = {
         body: `
         this._appGet('formData', function(ui, data) {
           ui.items = data;
-          ui.formData.textField2 = '2222222222222';
+          ui.formData.textField3 = '2222222222222';
           ui.formData.textField4 = '4444444444444';
           ui.getUserData();
         })`
       },
       submitForm: {
         args:[],
-        body: `this._appPost('submitform', this.formData, function(ui, data) { alert('result: '+data.success);});`
+        body: `this._appPost('submitform', this.formData, function(ui, data) {
+          console.log(data.success);
+        });`
       },
     };
 const components = {
   TableRow: {
     component: 'tr',
+    on: {click: "this.clickEvent"},
     contents: [
       {
         component: 'td',
-        template: '<span>{{scopedProps.firstName}}</span>'
+        template: '<span>{{item.firstName}}</span>'
       },
       {
         component: 'td',
-        template: '<span>{{scopedProps.lastName}}</span>'
+        template: '<span>{{item.lastName}}</span>'
       },
       {
         component: 'td',
-        template: '<span>{{scopedProps.address}}</span>'
+        template: '<span>{{item.address}}</span>'
       }
     ]
   }
 }
 
 const uiConfig = {
-      requiredUserData: ['textField1', 'textField3'],
-      components: components,
-      dataModel:dataModel,
-      uiMethods: uiMethods,
-      uiSchema: {
-        component: 'container',
-        contents: [
-          {
-            component: 'card',
-            props: {
-              elevation: 2
-            },
-            contents: [{
-              component: 'cardTitle',
+  requiredUserData: ['firstName', 'lastName'],
+  dataModel:dataModel,
+  uiMethods: uiMethods,
+  uiSchema: {
+    component: 'container',
+    contents: [
+      {
+        component: 'card',
+        props: {
+          elevation: 2
+        },
+        contents: [{
+          component: 'cardTitle',
 //              contents: 'This is the title'
-              template: '<span>Sample App: {{ch_userData.textField1}}</span>'
-            },
+          template: '<span>Welcome {{ch_userData.firstName}} {{ch_userData.lastName}}</span>'
+        },
+        {
+          component: 'cardBody',
+          contents: [
             {
-              component: 'cardBody',
+              component: 'form',
+              props: {
+                'lazy-validation': true
+              },
               contents: [
                 {
-                  component: 'form',
-                  props: {
-                    'lazy-validation': true
-                  },
+                  component: 'row',
                   contents: [
                     {
-                      component: 'row',
+                      component: 'col',
+                      props: {  cols:12, md:6, sm:12},
                       contents: [
                         {
-                          component: 'col',
-                          props: {  cols:12, md:6, sm:12},
-                          contents: [
-                            {
-                              component: 'textField',
-                              vmodel: 'formData.textField1',
-                              tokenId: 'textField1',
-                              props: {
-                                dense: true,
-                                outlined: false,
-                                label: 'Field 1'
-                              }},
-                              {
-                              component: 'textField',
-                              vmodel: 'formData.textField2',
-                              props: {
-                                dense: true,
-                                outlined: false,
-                                label: 'Field 2'
-                              }}
-                          ]
-                        },
+                          component: 'textField',
+                          vmodel: 'formData.firstName',
+                          tokenId: 'firstName',
+                          props: {
+                            dense: true,
+                            outlined: false,
+                            label: 'First Name'
+                          }},
+                          {
+                          component: 'textField',
+                          vmodel: 'formData.lastName',
+                          tokenId: 'lastName',
+                          props: {
+                            dense: true,
+                            outlined: false,
+                            label: 'Last Name'
+                          }}
+                      ]
+                    },
+                    {
+                      component: 'col',
+                      props: { cols:12, md:6, sm:12},
+                      contents: [
                         {
-                          component: 'col',
-                          props: { cols:12, md:6, sm:12},
-                          contents: [
-                            {
-                              component: 'textField',
-                              vmodel: 'formData.textField3',
-                              tokenId: 'textField3',
-                              props: {
-                                dense: true,
-                                outlined: false,
-                                label: 'Field 3'
-                              }},
-                              {
-                              component: 'textField',
-                              vmodel: 'formData.textField4',
-                              props: {
-                                dense: true,
-                                outlined: false,
-                                label: 'Field 4'
-                              }}
-                          ]
-                        }
+                          component: 'textField',
+                          vmodel: 'formData.textField3',
+                          props: {
+                            dense: true,
+                            outlined: false,
+                            label: 'Field 3'
+                          }},
+                          {
+                          component: 'textField',
+                          vmodel: 'formData.textField4',
+                          props: {
+                            dense: true,
+                            outlined: false,
+                            label: 'Field 4'
+                          }}
                       ]
                     }
                   ]
                 }
               ]
-            },
-            {
-              component: 'cardActions',
-              contents: [{
-                component: 'button',
-                contents: 'Cancel'
-              },
-              {
-                component: 'spacer'
-              },
-              {
-                component: 'button',
-                on: {
-                  click: 'this.submitForm'
-                },
-                contents: 'Save'
-              }
-              ]
-            }]
-          },
-          {
-            component: 'tabs',
-            props: {
-              'align-with-title': true
-            },
-            vmodel: "tab",
-            contents: [
-              {
-                component: 'tabsSlider',
-                props:{ color: "yellow"}
-              },
-              {
-                component: 'tab',
-                template: '<span>{{tabItems[0]}}</span>'
-              },
-              {
-                component: 'tab',
-                template: '<span>{{tabItems[1]}}</span>'
-              },
-              {
-                component: 'tab',
-                template: '<span>{{tabItems[2]}}</span>'
-              }
-            ]
-          },
-          {
-            component: 'tabsItems',
-            vmodel: "tab",
-            contents: [
-              {
-                component: 'tabItem',
-                contents: [
-                  {
-                    component: 'dataTable',
-                    attrs: {
-                      uiSchema: components.TableRow,
-                      headers: "this.headers",
-                      items: "this.items"
-                    }
-                  }        
-                ]
-              },
-              {
-                component: 'tabItem',
-                contents: 'Contents of Tab 2'
-              },
-              {
-                component: 'tabItem',
-                contents: 'Contents of Tab 3'
-              }
-            ]
-          },
-          {
-            component: 'conversation',
-            attrs: {
-              topic: 'Test Topic'
             }
+          ]
+        },
+        {
+          component: 'cardActions',
+          contents: [{
+            component: 'button',
+            contents: 'Cancel'
+          },
+          {
+            component: 'spacer'
+          },
+          {
+            component: 'button',
+            on: {
+              click: 'this.submitForm'
+            },
+            contents: 'Save'
+          }
+          ]
+        }]
+      },
+      {
+        component: 'tabs',
+        props: {
+          'align-with-title': true
+        },
+        vmodel: "tab",
+        contents: [
+          {
+            component: 'tabsSlider',
+            props:{ color: "yellow"}
+          },
+          {
+            component: 'tab',
+            template: '<span>{{tabItems[0]}}</span>'
+          },
+          {
+            component: 'tab',
+            template: '<span>{{tabItems[1]}}</span>'
+          },
+          {
+            component: 'tab',
+            template: '<span>{{tabItems[2]}}</span>'
           }
         ]
+      },
+      {
+        component: 'tabsItems',
+        vmodel: "tab",
+        contents: [
+          {
+            component: 'tabItem',
+            contents: [
+              {
+                component: 'dataTable',
+                attrs: {
+                  uiSchema: components.TableRow,
+                  headers: "this.headers",
+                  items: "this.items"
+                }
+              }        
+            ]
+          },
+          {
+            component: 'tabItem',
+            contents: 'Contents of Tab 2'
+          },
+          {
+            component: 'tabItem',
+            contents: 'Contents of Tab 3'
+          }
+        ]
+      },
+      {
+        component: 'conversation',
+        attrs: {
+          topic: 'Test Topic'
+        }
       }
-    };
+    ]
+  }
+};
+const uiConfig2 = {
+  requiredUserData: ['firstName', 'lastName'],
+  dataModel:{},
+  uiMethods: {},
+  uiSchema: {
+    component: 'container',
+    contents: [
+      {
+        component: 'card',
+        props: {
+          elevation: 2
+        },
+        contents: [{
+          component: 'cardTitle',
+          template: '<span>Form submitted for {{ch_userData.firstName}} {{ch_userData.lastName}}</span>'
+        },
+        {
+          component: 'cardBody',
+          contents: "More to come..."
+        }],
+      }
+    ]
+  }
+};
+
 
 import { Router } from 'express';
 
@@ -228,6 +255,9 @@ export default class VendorApp {
     this.router.get("/initUIConfig", (req, res) => {
       res.json(uiConfig)
     });
+    this.router.get("/formSubmitted", (req, res) => {
+      res.json(uiConfig2)
+    });
     this.router.get("/formData", (req, res) => {
       res.json([
         {firstName:'Bob', lastName: 'Smith', address: '1234 Bob Street, Bob City, CA 99999'},
@@ -238,7 +268,7 @@ export default class VendorApp {
 
     this.router.post("/submitform", (req, res) => {
       console.log('Form data:\n'+JSON.stringify(req.body));
-      res.json({success:true})
+      res.json({success:true, newPage:'formSubmitted'})
     });
 
     return this.router;
