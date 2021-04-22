639cf2532917df87828a9195a8f779848f60f1e1
richjvann <richjvann@gmail.com>
2021-04-22 15:03:04 -0700

    checkpoint save of demo apps

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,6 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import User from "../models/user";
 import { CalendarScheduling } from './actions/calendarscheduling.js';
-import VendorApp from './actions/vendorapp.js'
 import { UserDataMgr } from './actions/userdata.js'
 import { ConversationMgr } from './actions/conversation.js'
 import { UserInfo } from './actions/userinfo.js'
@@ -100,7 +99,6 @@ export default function() {
     router.use( '/auditlog', new AuditLogReview().route());
     router.use( '/eventlog', new EventLogReview().route());
 
-    router.use( '/vendorapp', new VendorApp().route());
     router.use( '/userinfo', new UserInfo().route());
     router.use( '/usersearch', new UserSearch().route());
     router.use( '/userdata', new UserDataMgr().route());
